c97c6fadb13348bcb5edf34f6c72e1aaf28a306f
GevorgyanSam <mylaptopacer.01182005@gmail.com>
2022-09-02 12:04:27 +0300

    Fixing some bugs on responsive navbar

--- src/js/script.js
+++ src/js/script.js
@@ -14,18 +14,9 @@ $(function() {
 
     }
 
-    function mobileMenu() {
-
-        $(".mobileNavParent").css({
-            top: $(window).scrollTop(),
-        })
-
-    }
-
     $(window).scroll(() => {
 
         navBar()
-        mobileMenu()
 
     })
     
@@ -34,25 +25,90 @@ $(function() {
     links.click(function() {
         links.removeClass("active");
         $(this).addClass("active")
+
+        mobileNavBackground.css({
+            opacity: "0",
+        })
+
+        mobileNavParent.css({
+            left: "-1500px",
+        })
+
+        setTimeout(function() {
+
+        mobileNavBackground.css({
+            display: "none",
+        })
+
+        }, 200)
     })
 
     const navBtn = $(".navBtn");
+    const mobileNavBackground = $(".mobileNavBackground");
     const mobileNavParent = $(".mobileNavParent");
     const closeMobile = $(".closeMobile");
 
     navBtn.click(() => {
 
-        mobileNavParent.css({
-            left: "0px",
+        mobileNavBackground.css({
+            display: "block",
         })
+
+        setTimeout(function() {
+
+            mobileNavBackground.css({
+                opacity: "1",
+            })
+
+            mobileNavParent.css({
+                left: "0px",
+            })
+
+        }, 100)
 
     })
 
     closeMobile.click(() => {
 
+        mobileNavBackground.css({
+            opacity: "0",
+        })
+
         mobileNavParent.css({
             left: "-1500px",
         })
+
+        setTimeout(function() {
+
+        mobileNavBackground.css({
+            display: "none",
+        })
+
+        }, 200)
+
+    })
+
+    mobileNavBackground.click(() => {
+
+        mobileNavParent.click(function() {
+            return false;
+        })
+
+        mobileNavBackground.css({
+            opacity: "0",
+        })
+
+        mobileNavParent.css({
+            left: "-1500px",
+        })
+
+        setTimeout(function() {
+
+        mobileNavBackground.css({
+            display: "none",
+        })
+
+        }, 200)
 
     })
 
